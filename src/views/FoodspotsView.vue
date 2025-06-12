3a1f102439fdcabaf88a5570ad93f0944b0a5656
quocAnhnguyen1 <Quoc.Nguyen@Student.htw-berlin.de>
2025-06-12 21:23:58 +0200

    test 2

--- src/views/FoodspotsView.vue
+++ src/views/FoodspotsView.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import FoodspotList from "@/components/FoodspotList.vue"
+
+const someMethod = (): void => {
+  console.log('Hello There!')
+}
 </script>
+
 
 <template>
   <main>
